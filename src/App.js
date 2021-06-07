bc776178ff9a69e975af4a2d99e4120217538297
Utku <utkumukan@gmail.com>
2021-06-07 20:19:58 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+
+const Child = (props) => {
+  const { count } = props;
+  const [input, setInput] = useState("hadi bir şeyler yaz buraya");
+
+  useEffect(() => {
+    console.log(
+      "count ve input değerlerinin mount ve update durumlarında çalışırım"
+    );
+  }, [count, input]);
+
+  return (
+    <div className="Child">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <h2>{count}</h2>
+    </div>
+  );
+};
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleMinus = () => {
+    setCount(count - 1);
+  };
+
+  const handlePlus = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div className="App">
+      <button onClick={handleMinus}>-</button>
+      <Child count={count} />
+      <button onClick={handlePlus}>+</button>
+    </div>
+  );
+};
+
+export default App;
